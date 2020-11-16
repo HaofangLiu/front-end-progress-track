b8c643e09dab953ed8908a0a0ee2d3deae044b22
Lucas-Liu <haofanglucas@gmail.com>
2020-11-16 09:41:57 +0800

    curry function and basic function

--- js深入浅出/柯里化函数.js
+++ js深入浅出/柯里化函数.js
@@ -1,8 +1,12 @@
-function curry(func) {
+function curry(func) { // 接受需要执行的函数之前先进行柯里化
+
+  // 已经完成柯里化，可以执行函数
   return function curried(...args) {
+    // 当接受的参数长度和需要执行的函数的参数相等或者大于时，执行函数
     if (args.length >= func.length) {
       return func.apply(this, args);
     } else {
+      // 否则，合并参数（执行柯里化步骤）
       return function (...args2) {
         return curried.apply(this, args.concat(args2));
       };
@@ -10,6 +14,7 @@ function curry(func) {
   };
 }
 
+// 完全可以自定义来执行不同的任务
 function sum(a, b, c) {
   return a + b + c;
 }
